2fae450341fc3cb8607c56bab052dabc238b8c84
rudenko-vl <vladrudenko7@gmail.com>
2021-10-21 15:10:25 +0300

    markup and connection of libraries

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector("div.gallery");
+
+galleryDiv.insertAdjacentHTML(
+  "beforeend",
+  createGalleryItemsMarkup(galleryItems)
+);
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGallery = document.querySelector("ul.gallery");
+
+listGallery.insertAdjacentHTML(
+  "beforeend",
+  createGalleryItemsMarkup(galleryItems)
+);
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
